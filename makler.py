6c5d7f9c919032caac912645a1daaf55942b24da
guoepl <jerome.d3@gso.schule.koeln>
2022-03-09 19:29:09 +0100

    many changes

--- makler.py
+++ makler.py
@@ -1,67 +1,79 @@
 from math import sin, cos, pi
 
 
-def flat_size():
-    area = 0
-    is_valid_number_of_rooms = False
-    while not is_valid_number_of_rooms:
+def create_points(walls:list) -> list:
+    angle, last_angle = 0, 0
+    points = [(0, 0), (walls[0][1], 0)]
+    for k, j in enumerate(walls[1:-1]):
+        angle = (last_angle + 180 - walls[k][2]) % 360
+        last_angle = angle
+        x = points[k + 1][0] + round(j[1] * cos((pi / 180) * angle), 2)
+        y = points[k + 1][1] + round(j[1] * sin((pi / 180) * angle), 2)
+        points.append((x, y))
+    return points
+
+
+def calc_area(points:list) -> list:
+    """
+    Calculate the area of given points.
+    """
+    number_of_points = len(points)
+    sum1, sum2 = 0, 0
+    for i in range(number_of_points):
+        if i == number_of_points - 1:
+            sum1 = sum1 + points[i][0] * points[0][1]
+            sum2 = sum2 + points[0][0] * points[i][1]
+        else:
+            sum1 = sum1 + points[i][0] * points[i + 1][1]
+            sum2 = sum2 + points[i][1] * points[i + 1][0]
+    return abs(sum1 - sum2) / 2
+    
+
+def validate_input(choice: int):
+    """
+    Return the validated input of choice.
+    """
+    is_valid_input = False
+    while not is_valid_input:
         try:
-            number_of_rooms: int = int(input("Anzahl Räume: "))
+            if choice == 1:
+                valid_input = int(input("Anzahl Räume: "))
+            if choice == 2:
+                valid_input = int(input("Anzahl Wände: "))
+            if choice == 3:
+                valid_input = float(input("Wandbreite: "))
+            if choice == 4:
+                valid_input = float(input("Winkel: "))
         except ValueError:
             print("Ungültige Eingabe")
         else:
-            is_valid_number_of_rooms = True
+            is_valid_input = True
+    return valid_input    
+
+
+def flat_size() -> float:
+    area = 0
+    number_of_rooms = validate_input(1)
     for _ in range(number_of_rooms):
-        is_valid_number_of_walls = False
-        while not is_valid_number_of_walls:
-            try:
-                number_of_walls: int = int(input("Anzahl Wände: "))
-            except ValueError:
-                print("Ungültige Eingabe")
-            else:
-                is_valid_number_of_walls = True
+        number_of_walls = validate_input(2)
         walls = []
-        is_right_angled = True if input("Nur rechtwinkelig? j / n\n").lower().strip() == "j" else False
+        is_right_angled = True if input("Nur rechtwinkelig? j / n : ").lower().strip() == "j" else False
         for i in range(number_of_walls):
-            is_valid_wall_width, is_valid_angle = False, False
-            while not is_valid_wall_width:
-                try:
-                    wall_width: float = float(input("Wandbreite: "))
-                except ValueError:
-                    print("Ungültige Eingabe")
-                else:
-                    is_valid_wall_width = True
+            wall_width = validate_input(3)
             if is_right_angled:
                 angle = 90
             else:
-                while not is_valid_angle:
-                    try:
-                        angle = float(input("Winkel: "))
-                    except ValueError:
-                        print("Ungültige Eingabe")
-                    else:
-                        is_valid_angle = True
+                angle = validate_input(4)
             walls.append((i, wall_width, angle))
-        angle, last_angle = 0, 0
-        points = [(0, 0), (walls[0][1], 0)]
-        for k, j in enumerate(walls[1:-1]):
-            angle = (last_angle + 180 - walls[k][2]) % 360
-            last_angle = angle
-            x = points[k + 1][0] + round(j[1] * cos((pi / 180) * angle), 2)
-            y = points[k + 1][1] + round(j[1] * sin((pi / 180) * angle), 2)
-            points.append((x, y))
-        number_of_points = len(points)
-        sum1, sum2 = 0, 0
-        for i in range(number_of_points):
-            if i == number_of_points - 1:
-                sum1 = sum1 + points[i][0] * points[0][1]
-                sum2 = sum2 + points[0][0] * points[i][1]
-            else:
-                sum1 = sum1 + points[i][0] * points[i + 1][1]
-                sum2 = sum2 + points[i][1] * points[i + 1][0]
-        area = area + abs(sum1 - sum2) / 2
+        points = create_points(walls)
+        area += calc_area(points)   
+    return area
+
+
+def main():
+    area = flat_size()
     print(f"Die Wohnung ist {area} qm groß.")
 
 
 if __name__ == '__main__':
-    flat_size()
+    main()
