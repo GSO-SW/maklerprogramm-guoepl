6f70b9c0b67d4af429748e5d17a37061740373ce
guoepl <jerome.d3@gso.schule.koeln>
2022-03-10 20:30:19 +0100

    recu and more

--- makler.py
+++ makler.py
@@ -3,7 +3,7 @@ from math import sin, cos, pi
 
 def create_points(walls:list) -> list:
     angle, last_angle = 0, 0
-    points = [(0, 0), (walls[0][1], 0)]
+    points: list = [(0, 0), (walls[0][1], 0)]
     for k, j in enumerate(walls[1:-1]):
         angle = (last_angle + 180 - walls[k][2]) % 360
         last_angle = angle
@@ -17,7 +17,7 @@ def calc_area(points:list , multiplier: float = 1.0) -> float:
     """
     Calculate the area of given points.
     """
-    number_of_points = len(points)
+    number_of_points: int = len(points)
     sum1, sum2 = 0, 0
     for i in range(number_of_points):
         if i == number_of_points - 1:
@@ -47,13 +47,20 @@ def validate_input(choice: int, msg: str):
     return valid_input    
 
 
-def flat_size() -> float:
-    area = 0
-    number_of_rooms = validate_input(1, "Anzahl Räume: ")
+def flat_size(msg1: str = "Anzahl Räume: ", reduced: bool = False) -> float:
+    area: float = 0
+    number_of_rooms: int = validate_input(1, msg1)
     for _ in range(number_of_rooms):
-        walls = []
+        is_partially_reduced = False
+        multiplier = 1.0
+        walls: list = []
         number_of_walls = validate_input(1, "Anzahl Wände: ")
         is_right_angled = True if input("Nur rechtwinkelig? j / n : ").lower().strip() == "j" else False
+        if not reduced:
+            is_partially_reduced = True if input("Teilbereiche des Raumes müssen" +\
+                " mit verringerten Maßen berechnet werden? j / n : ").lower().strip() == "j" else False
+        else:
+            multiplier = validate_input(2, "Multiplier: ")
         for i in range(number_of_walls):
             wall_width = validate_input(2, "Wandbreite: ")
             if is_right_angled:
@@ -62,7 +69,9 @@ def flat_size() -> float:
                 angle = validate_input(2, "Winkel: ")
             walls.append((i, wall_width, angle))
         points = create_points(walls)
-        area += calc_area(points)   
+        area += calc_area(points, multiplier)
+        if is_partially_reduced:
+            area -= flat_size("Anzahl Teilbereiche mit reduzierter Fläche: ", True)
     return area
 
 
